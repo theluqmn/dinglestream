aa2fe4e717ccf818de59c2ee0d462fbd6537b1b7
luq-mne <luqman.yazrie@outlook.com>
2024-11-21 19:17:16 +0800

    started work on logging

---
+++ src/utils/logging.js
@@ -0,0 +1,16 @@
+function GetTime() {
+    const date = new Date()
+    let hours = date.getHours()
+    let minutes = date.getMinutes()
+    let seconds = date.getSeconds()
+    let milliseconds = date.getMilliseconds().toString().padStart(3, '0').slice(0, 2)
+    let time = `${hours}:${minutes}:${seconds}.${milliseconds}`
+
+    return time
+}
+
+export function LogInfo(message) {
+    console.log(`[${GetTime()}][INFO] ${message}`)
+}
+
+console.log(LogInfo("test"))
